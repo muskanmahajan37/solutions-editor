3c310fb3afc01fc28858f6aceaadf6dcae494333
Fengda HUANG <h@phodal.com>
2016-05-26 09:05:34 +0800

    add example code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,30 @@
 console.log('\'Allo \'Allo!');
+
+var token = localStorage.getItem('token');
+var username = localStorage.getItem('repo').split("/")[0];
+var reponame = localStorage.getItem('repo').split("/")[1];
+
+var github = new GitHub({
+  token: token,
+  auth: "oauth"
+});
+
+var repo = github.getRepo(username, reponame);
+
+var options = {
+  author: {name: 'Phodal Robot', email: 'robot@phodal.com'},
+  committer: {name: 'Phodal Robot', email: 'robot@phodal.com'},
+  encode: true
+};
+
+var data = {
+
+};
+
+var stringifyData = JSON.stringify(data);
+
+repo.write('master', 'content/' + data.url + '.json', stringifyData, 'Robot: add article ' + data.title, options, function (err, data) {
+  if (data.commit) {
+    console.log("Commit Success");
+  }
+});
