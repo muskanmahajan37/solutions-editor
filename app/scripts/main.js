da69d24739863eab37cbf3666859d2e60341a583
Fengda HUANG <h@phodal.com>
2016-05-27 07:14:55 +0800

    extract method

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,29 @@
-var token = localStorage.getItem('token');
+function push() {
+  var token = localStorage.getItem('token');
 
-var github = new GitHub({
-  token: token
-});
+  var github = new GitHub({
+    token: token
+  });
 
-var repo = github.getRepo('phodal', 'solutions-content');
+  var repo = github.getRepo('phodal', 'solutions-content');
 
-var options = {
-  author: {name: 'Phodal', email: 'h@phodal.com'},
-  committer: {name: 'Phodal', email: 'h@phodal.com'},
-  encode: true
-};
+  var options = {
+    author: {name: 'Phodal', email: 'h@phodal.com'},
+    committer: {name: 'Phodal', email: 'h@phodal.com'},
+    encode: true
+  };
 
-var data = {
-  title: 'hybird-app'
-};
+  var data = {
+    title: 'hybird-app'
+  };
 
-var stringifyData = JSON.stringify(data);
+  var stringifyData = JSON.stringify(data);
 
-repo.writeFile('master', 'content/' + data.title + '.json', stringifyData, 'Robot: add article ' + data.title, options, function (err, data) {
-  if (data.commit) {
-    console.log("Commit Success");
-  }
-});
+  repo.writeFile('master', 'content/' + data.title + '.json', stringifyData, 'Robot: add article ' + data.title, options, function (err, data) {
+    if (data.commit) {
+      console.log("Commit Success");
+    }
+  });
+}
+
+// push();
