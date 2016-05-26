687cbe781f6a551eaeb40ec70c727500acb1e3ac
Fengda HUANG <h@phodal.com>
2016-05-27 00:14:53 +0800

    create file successful

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,24 @@
-console.log('\'Allo \'Allo!');
-
 var token = localStorage.getItem('token');
-var username = localStorage.getItem('repo').split("/")[0];
-var reponame = localStorage.getItem('repo').split("/")[1];
 
 var github = new GitHub({
-  token: token,
-  auth: "oauth"
+  token: token
 });
 
-var repo = github.getRepo(username, reponame);
+var repo = github.getRepo('phodal', 'solutions-content');
 
 var options = {
-  author: {name: 'Phodal Robot', email: 'robot@phodal.com'},
-  committer: {name: 'Phodal Robot', email: 'robot@phodal.com'},
+  author: {name: 'Phodal', email: 'h@phodal.com'},
+  committer: {name: 'Phodal', email: 'h@phodal.com'},
   encode: true
 };
 
 var data = {
-
+  title: 'hybird-app'
 };
 
 var stringifyData = JSON.stringify(data);
 
-repo.write('master', 'content/' + data.url + '.json', stringifyData, 'Robot: add article ' + data.title, options, function (err, data) {
+repo.writeFile('master', 'content/' + data.title + '.json', stringifyData, 'Robot: add article ' + data.title, options, function (err, data) {
   if (data.commit) {
     console.log("Commit Success");
   }
