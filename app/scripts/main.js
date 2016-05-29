bc16923e768ee287ed2d04ab221aa761fb09f1d3
Fengda HUANG <h@phodal.com>
2016-05-30 07:50:30 +0800

    make form postable

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-function push() {
+function push(data) {
   var token = localStorage.getItem('token');
 
   var github = new GitHub({
@@ -13,10 +13,6 @@ function push() {
     encode: true
   };
 
-  var data = {
-    title: 'hybird-app'
-  };
-
   var stringifyData = JSON.stringify(data);
 
   repo.writeFile('master', 'content/' + data.title + '.json', stringifyData, 'Robot: add article ' + data.title, options, function (err, data) {
@@ -26,18 +22,38 @@ function push() {
   });
 }
 
-// push();
+var editor = new MediumEditor('.editable', {
+  buttonLabels: 'fontawesome',
+  extensions: {
+    table: new MediumEditorTable()
+  },
+  toolbar: {
+    allowMultiParagraphSelection: true,
+    buttons: ['bold', 'italic', 'fontname', 'underline', 'anchor', 'h2', 'h3', 'quote', 'table', 'unorderedlist', 'orderedlist'],
+    diffLeft: 0,
+    diffTop: -10,
+    firstButtonClass: 'medium-editor-button-first',
+    lastButtonClass: 'medium-editor-button-last',
+    standardizeSelectionStart: false,
+    static: false,
+    relativeContainer: null,
+    align: 'left',
+    sticky: false,
+    updateOnEmptySelection: false
+  }
+});
 
-$(document).ready(function(){
+$(document).ready(function () {
   var stacks = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     prefetch: {
       url: 'assets/stacks.json',
-      filter: function(list) {
+      filter: function (list) {
         console.log(list);
-        return $.map(list, function(stack) {
-          return { name: stack }; });
+        return $.map(list, function (stack) {
+          return {name: stack};
+        });
       }
     }
   });
@@ -51,29 +67,16 @@ $(document).ready(function(){
       source: stacks.ttAdapter()
     }
   });
-  $('.jumbotron').validator()
-});
+  $('.jumbotron').validator();
+  $('form#solution-form').on('submit', function (event) {
+    var paramObj = {};
+    $.each($('form#solution-form').serializeArray(), function (_, kv) {
+      paramObj[kv.name] = kv.value;
+    });
 
-var editor = new MediumEditor('.editable', {
-  buttonLabels: 'fontawesome',
-  extensions: {
-    table: new MediumEditorTable()
-  },
-  toolbar: {
-    /* These are the default options for the toolbar,
-     if nothing is passed this is what is used */
-    allowMultiParagraphSelection: true,
-    buttons: ['bold', 'italic', 'fontname', 'underline', 'anchor', 'h2', 'h3', 'quote', 'table', 'unorderedlist', 'orderedlist'],
-    diffLeft: 0,
-    diffTop: -10,
-    firstButtonClass: 'medium-editor-button-first',
-    lastButtonClass: 'medium-editor-button-last',
-    standardizeSelectionStart: false,
-    static: false,
-    relativeContainer: null,
-    /* options which only apply when static is true */
-    align: 'left',
-    sticky: false,
-    updateOnEmptySelection: false,
-  }
+    paramObj.content = editor.serialize()['element-0'].value;
+
+    push(paramObj);
+    event.preventDefault();
+  });
 });
